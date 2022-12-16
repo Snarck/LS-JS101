e5a9e3f554cd7b375a440c5439270907148ab05a
Snicks <isdragonirl@outlook.com>
2022-12-16 15:41:00 -0500

    Added language support to Calculator

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,10 +1,11 @@
-/*
-Ask the user for the first number, second number and operation.
-Perform the operation on the two numbers.
-print the results to the terminal.
-*/
-
+const msg = require('./calculator_messages.json');
 const rl = require('readline-sync');
+let lang = "en";
+
+function lng(message, lang='en') {
+  return msg[lang][message];
+}
+
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -14,51 +15,75 @@ function invalidNumber(num) {
   return num.trimStart() === '' || Number.isNaN(Number(num));
 }
 
-
-prompt("Welcome to the calculator!");
-
-prompt("What is your first number?\n");
-let number1 = rl.question();
-
-while (invalidNumber(number1)) {
-  prompt("Invalid number");
-  number1 = rl.question();
+prompt(lng("language", lang))
+if(rl.question() === "2") {
+  lang = "es";
 }
 
-prompt("What is your second number?\n");
-let number2 = rl.question();
+prompt(lng("greeting", lang));
 
-while (invalidNumber(number2)) {
-  prompt("Invalid number");
-  number2 = rl.question();
-}
 
-prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide\n");
-let operation = rl.question();
+while(true) {
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("Must be a valid operation\n1) Add 2) Subtract 3) Multiply 4) Divide\n");
-  operation = rl.question();
-}
+  prompt(lng("firstNum", lang));
+  let number1 = rl.question();
 
-let output;
-switch (operation) {
-  case "1":
-    output = Number(number1) + Number(number2);
+  while (invalidNumber(number1)) {
+    prompt(lng("notValid", lang));
+    number1 = rl.question();
+  }
+
+  prompt(lng("secondNum", lang));
+  let number2 = rl.question();
+
+  while (invalidNumber(number2)) {
+    prompt(lng("notValid", lang));
+    number2 = rl.question();
+  }
+
+  prompt(lng("operator", lang));
+  let operation = rl.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(lng("notValid", lang));
+    operation = rl.question();
+  }  
+
+  let output;
+  switch (operation) {
+    case "1":
+      output = Number(number1) + Number(number2);
+      break;
+    case "2":
+      output = Number(number1) - Number(number2);
+      break;
+    case "3":
+      output = Number(number1) * Number(number2);
+      break;
+    case "4":
+      output = Number(number1) / Number(number2);
+      break;
+    default:
+      prompt(lng("notValid", lang))
+  }
+  
+  if (output) {
+    prompt(`${lng("results", lang)} ${output}`);
+  }
+
+  prompt(lng("tryAgain", lang));
+  let answer = rl.question();
+  while(!["y", "n"].includes(answer[0].toLowerCase())) {
+    prompt(lng("notValid", lang));
+    answer = rl.question();
+   }
+  if(answer[0].toLowerCase() !== "y") {
+    prompt(lng("goodbye", lang));
     break;
-  case "2":
-    output = Number(number1) - Number(number2);
-    break;
-  case "3":
-    output = Number(number1) * Number(number2);
-    break;
-  case "4":
-    output = Number(number1) / Number(number2);
-    break;
-  default:
-    console.log("Not valid, sorry!");
+  }
+
 }
 
-if (output) {
-  prompt(`The result is: ${output}`);
-}
+
+
+
