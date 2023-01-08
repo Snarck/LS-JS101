4822617999429cf099ca0debfc6540755a134d2b
Snicks <isdragonirl@outlook.com>
2023-01-08 17:43:29 -0500

    Tic-Tac v.2

--- JS101/lesson_6/tic_tac_toe.js
+++ JS101/lesson_6/tic_tac_toe.js
@@ -1,14 +1,14 @@
 const READLINE = require('readline-sync');
 
+//Board Manipulation and intializiation
 function displayBoard(board) {
   const BOARD_OUROBOROS = ("");
   const BOARD_VERT = ('     |     |');
   const BOARD_HOR = ('-----+-----+-----');
-  let topRow = `  ${board[1].val}  |  ${board[2].val}  |   ${board[3].val}  `;
-  let midRow = `  ${board[4].val}  |  ${board[5].val}  |   ${board[6].val}  `;
-  let botRow = `  ${board[7].val}  |  ${board[8].val}  |   ${board[9].val}  `;
+  let topRow = `  ${board[1]}  |  ${board[2]}  |   ${board[3]}  `;
+  let midRow = `  ${board[4]}  |  ${board[5]}  |   ${board[6]}  `;
+  let botRow = `  ${board[7]}  |  ${board[8]}  |   ${board[9]}  `;
 
-  // console.clear();
   let boardOutput = [
     BOARD_OUROBOROS,
     BOARD_VERT, topRow, BOARD_VERT,
@@ -21,36 +21,81 @@ function displayBoard(board) {
   boardOutput.forEach(element => console.log(element));
 }
 
+function initializeBoard() {
+
+  let board = {
+    1 : "1",
+    2 : "2",
+    3 : "3",
+    4 : "4",
+    5 : "5",
+    6 : "6",
+    7 : "7",
+    8 : "8",
+    9 : "9",
+    userMark : "X",
+    cpuMark : "O",
+    legalSquares : ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  };
+  return board;
+}
+
+function initalizeScore() {
+  let score = {
+    userScore : 0,
+    cpuScore : 0,
+    winsNeeded : 2,
+    tie : 0,
+  };
+  return score;
+}
+
+function displayScore(score) {
+  prompt(`Player Wins: ${score.userScore} Computer Wins: ${score.cpuScore}`);
+  prompt(`Number of matches resulting in ties: ${score.tie}`);
+  prompt(`Playing until someone wins ${score.winsNeeded} games.`);
+}
+
+function updateLegalSquares(board, square) {
+  let removedSquare = board.legalSquares.indexOf(square);
+  board.legalSquares.splice(removedSquare, 1);
+}
+
+function gameHeader(board, score) {
+  prompt(`You are ${board.userMark}. Computer is ${board.cpuMark}`);
+  displayScore(score);
+  displayBoard(board);
+} //Done handling Board Manipulation and Initalization
+
+
+//Handling input/output/choices
 function prompt(output) {
   console.log(`>>> ${output}`);
 }
 
-function initializeBoard() {
+function joinOr(array, delimiter, joinWord) {
+  switch (array.length) {
+    case 0 :
+      return "";
+    case 1 :
+      return array[0];
+    case 2 :
+      return array.join(` ${joinWord} `);
+    default :
+      return array.slice(0, array.length - 1).join(delimiter) +
+             `${delimiter + joinWord} ${array[array.length - 1]}`;
 
-  const board = {
-    1 : {val : "X" , avail : true},
-    2 : {val : "O" , avail : true},
-    3 : {val : "X" , avail : true},
-    4 : {val : "O" , avail : true},
-    5 : {val : "X" , avail : true},
-    6 : {val : "O" , avail : true},
-    7 : {val : "X" , avail : true},
-    8 : {val : "O" , avail : true},
-    9 : {val : "X" , avail : true},
-  };
-  for (let idx = 1; idx < 10; idx += 1 ) {
-    board[idx].val = idx;
   }
-  return board;
 }
 
-function playerChoosesSquare(board) {
-
+function userChoosesSquare(board) {
   while (true) {
-    prompt(`Please choose a square: ${getRemainingSquares(board).join(" ")}.`);
+    prompt(`Please choose a square: ${joinOr(board.legalSquares, ", ", "or")}.`);
     let square = READLINE.question();
-    if (board[square].avail) {
-      updateBoard(square, "X", board);
+
+    if (board.legalSquares.includes(square)) {
+      updateLegalSquares(board, square);
+      board[square] = board.userMark;
       break;
     } else {
       prompt(`Invalid number : ${square}`);
@@ -58,47 +103,126 @@ function playerChoosesSquare(board) {
   }
 }
 
-function getRemainingSquares(board) {
-  let remainingSquares = [];
-  for (let idx = 1; idx < 10; idx += 1) {
-    if (board[idx].avail === true) {
-      remainingSquares.push(idx);
+function alternatePlayer(currentPlayer = "Player") {
+  return currentPlayer === "Player" ? "Computer" : "Player";
+}
+
+function chooseSquare(board, currentPlayer) {
+  if (currentPlayer === "Player") {
+    userChoosesSquare(board);
+  } else {
+    cpuChoosesSquare(board);
+  }
+}
+
+function playingAgain() {
+  const REGEX = new RegExp("^[YN]$", "i");
+  prompt("Would you like to play again? Y/N");
+
+  while (true) {
+    let userInput = READLINE.question();
+    if (REGEX.test(userInput)) {
+      return userInput === "Y" || userInput === "y";
+    } else {
+      prompt("Sorry, only accepting Y/N");
+    }
+
+  }
+} //Done handling input/output/choices.
+
+//Handling computer turn:
+function cpuChoosesSquare(board) {
+  let cpuSquare =
+  board.legalSquares[Math.floor(Math.random() * board.legalSquares.length)];
+
+  updateLegalSquares(board, cpuSquare);
+  board[cpuSquare] = board.cpuMark;
+
+}
+
+
+//Resolving the game/score keeping.
+function boardFull(board) {
+  return board.legalSquares.length < 1;
+}
+
+function detectWinner(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+    [1, 5, 9], [3, 5, 7]             // diagonals
+  ];
+
+  for (let line = 0; line < winningLines.length; line += 1) {
+    let [sq1, sq2, sq3] = winningLines[line];
+    if ( board[sq1] === board.userMark &&
+         board[sq2] === board.userMark &&
+         board[sq3] === board.userMark)  {
+      return "Player";
+    } else if ( board[sq1] === board.cpuMark &&
+                board[sq2] === board.cpuMark &&
+                board[sq3] === board.cpuMark) {
+      return 'Computer';
     }
   }
-  return remainingSquares;
+  return null;
 }
 
-function updateBoard(square, playerSign, board) {
-  if (boardFull(board)) return;
-  board[square].val = playerSign;
-  board[square].avail = false;
+function someoneWon(board) {
+  return !!detectWinner(board);
 }
 
-function boardFull(board) {
-  if (getRemainingSquares(board).length === 0) {
-    return true;
+function scoreKeeper(score, winner) {
+  if (winner === "Player") {
+    score.userScore += 1;
+  } else if (winner === "Computer") {
+    score.cpuScore += 1;
+  } else {
+    score.tie += 1;
   }
-  return false;
-}
-
-function computerChoosesSquare(board) {
-  if (boardFull(board)) return;
-
-  let remainingSquares = getRemainingSquares(board);
-  let compSquare = Math.floor(Math.random() * remainingSquares.length);
-  compSquare = remainingSquares[compSquare];
-  updateBoard(compSquare, "O", board);
-  prompt(`Computer chose ${compSquare}`);
 
 }
-let board = initializeBoard();
-displayBoard(board);
+
+function matchOver(score) {
+  if (score.userScore === score.winsNeeded) {
+    prompt("Player wins the match!");
+    return true;
+  } else if (score.cpuScore === score.winsNeeded) {
+    prompt("Computer wins the match!");
+    return true;
+  } else {
+    return false;
+  }
+
+} // Done Resolving the game/score keeping.
+
+//menu loop
 while (true) {
+  let score = initalizeScore();
 
-  playerChoosesSquare(board);
-  computerChoosesSquare(board);
-  displayBoard(board);
-  if (boardFull(board)) break;
+  //main game loop
+  while (true) {
+    let board = initializeBoard();
+    let currentPlayer = "Player"; //Would like to make this an optional.
 
+    while (true) {
+      gameHeader(board, score);
+      chooseSquare(board, currentPlayer);
+      currentPlayer = alternatePlayer(currentPlayer);
+      if (boardFull(board) || someoneWon(board)) break;
+
+    }
+
+    if (someoneWon(board)) {
+      scoreKeeper(score, detectWinner(board));
+      if (matchOver(score)) {
+        break;
+      }
+    } else {
+      scoreKeeper(score, "Tie");
+    }
+  }
+  if (!playingAgain()) break;
 }
-console.log("Thanks for playing");
+
+prompt("Thanks for playing!");
